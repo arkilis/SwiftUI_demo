80ccecff3a7c870964c857f8771d75a9c566a7c9
Ben LIU <arkilis@gmail.com>
2020-08-26 08:43:43 +1000

    add mapview

---
+++ swiftUI Test/Views/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  swiftUI Test
+//
+//  Created by Ben Liu on 26/8/20.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    typealias UIViewType = .
+    
+    
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
